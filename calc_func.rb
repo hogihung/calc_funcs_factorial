7bf8052fbf06f5d5a13e2881b442f850b6a9f240
John Hogarty <hogihung@gmail.com>
2016-04-24 14:30:48 -0400

    Code solution to make the test suite pass for factorial.

--- calc_func.rb
+++ calc_func.rb
@@ -1,4 +1,10 @@
 class CalcFunc
   def factorial(num)
+    return "Silly Wabbit, can't calculate the factorial of a negative number" if num <= 0
+    if num <= 1
+      1
+    else
+      num * factorial(num -1)
+    end
   end
 end
