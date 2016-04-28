f2199702b427d336d057c9978a1e932913f92e55
John Hogarty <hogihung@gmail.com>
2016-04-28 11:39:48 -0400

    Update for new error message requirement.

--- calc_func.rb
+++ calc_func.rb
@@ -1,6 +1,7 @@
 class CalcFunc
   def factorial(num)
-    return "Silly Wabbit, can't calculate the factorial of a negative number" if num <= 0
+    return "Silly Wabbit, can't calculate the factorial of a negative number" if num < 0
+    return "Silly Wabbit, can't calculate the factorial for zero" if num == 0
     if num <= 1
       1
     else
